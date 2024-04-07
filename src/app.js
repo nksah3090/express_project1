884acc127c512a8ce93f917a0ce7bb85c0e7c1de
nksah3090 <nksah3090@gmail.com>
2024-04-07 23:06:51 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,85 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
+const { name } = require('ejs')
+
+const addData = function (name, need, mobile) {
+    const data = loadData();
+    data.push({
+        name: name,
+        need: need,
+        mobile: mobile
+
+    });
+    console.log("Name: " + name);
+    console.log("Need: " + need);
+    console.log("Mobile :" + mobile);
+    saveData(data);
+}
+
+const saveData = function (data) {
+    const dataJSON = JSON.stringify(data)
+    fs.writeFileSync('data.json', dataJSON)
+}
+
+const loadData = function () {
+    try {
+        const dataBuffer = fs.readFileSync('data.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
 
 
 const app = express()
 
-const PORT = process.env.PORT || 3030; 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// app.post('/formPost', (req, res) => {
+//     console.log(req.body);
+// })
+
+app.use(express.static('template/partials'));
+
+
+
+const PORT = process.env.PORT || 3030;
+
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../template/views')
 const partialsPath = path.join(__dirname, '../template/partials')
-const dataFilePath = path.join(__dirname, 'data', 'example.json')  
+const dataFilePath = path.join(__dirname, 'example.json')
+
+app.post('/submit-form', (req, res) => {
+    const formData = req.body;
+
+    // Load existing data from JSON file
+    let existingData = [];
+    try {
+        existingData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
+    } catch (err) {
+        console.error('Error reading file:', err);
+    }
+
+    // Add new form data to existing data
+    existingData.push(formData);
+
+    // Write updated data back to JSON file
+    fs.writeFile(dataFilePath, JSON.stringify(existingData, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            res.status(500).send('Error submitting form data');
+        } else {
+            res.status(200).send('Form data submitted successfully');
+        }
+    });
+});
 
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
@@ -23,23 +92,36 @@ app.get('/', (req, res) => {
     res.render('index', {
         title: 'NEED IT',
         name: 'Neeraj'
-        
+
+    })
+})
+
+app.post('/data', (req, res) => {
+    addData(req.body.name, req.body.need, req.body.mobile);
+    res.render('data', {
+        title: 'NEED IT',
+        name: 'Neeraj',
+        user: req.body.name,
+        userMobile: req.body.mobile
+
     })
 })
 
 
 
-app.get('/requirement', (req, res) => {
-    res.render('requirement', {
+app.get('/need', (req, res) => {
+    const data = loadData();
+    res.render('need', {
         helpText: '',
-        title: 'Requirement',
-        name: 'Neeraj'
+        title: 'Needs',
+        name: 'Neeraj',
+        data: data
     })
 })
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Us',
-        name:'Neeraj'
+        name: 'Neeraj'
     })
 })
 
